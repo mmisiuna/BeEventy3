56041b2921c9654ccb889aba60d869f118617849
mmisiuna <164675296+mmisiuna@users.noreply.github.com>
2024-08-24 21:36:58 +0200

    backend

---
+++ Frontend/src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EventListComponent } from './event-list/event-list.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AccountComponent } from './account/account.component';
+import { EventDetailsComponent } from './event-details/event-details.component';
+import { AddEventComponent } from './add-event/add-event.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: 'events', component: EventListComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'account', component: AccountComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'add-event', component: AddEventComponent },
+  { path: 'event-details/:id', component: EventDetailsComponent },
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
